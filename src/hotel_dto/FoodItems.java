aa1cf54ac234f5e3f956c3ad0492f80d58972d72
Karthik Ram Gudimetla <100182451+karthikram1269@users.noreply.github.com>
2025-02-27 22:29:05 +0530

    Update FoodItems.java

--- src/hotel_dto/FoodItems.java
+++ src/hotel_dto/FoodItems.java
@@ -4,36 +4,44 @@ public class FoodItems {
 	private String name;
 	private double price;
 	private int id;
-	
-	public FoodItems () {}
-	
+
+	public FoodItems() {
+	}
+
 	public FoodItems(String name, double price, int id) {
 		super();
 		this.name = name;
 		this.price = price;
 		this.id = id;
 	}
+
 	@Override
 	public String toString() {
 		return "Foodnames [name=" + name + ", price=" + price + ", id=" + id + "]";
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public double getprice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 }
